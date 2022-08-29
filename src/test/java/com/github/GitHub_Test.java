81f76676556014e4a95959dfe2fc93eff15518f3
Kamila Adigamova <uuueesi@gmail.com>
2022-08-30 01:21:26 +0300

    added test code

--- src/test/java/com/github/GitHub_Test.java
+++ src/test/java/com/github/GitHub_Test.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class GitHub_Test {
+package com.github;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GitHub_Test {
+
+    @BeforeAll
+    static void configure() {
+        Configuration.baseUrl = "https://github.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.browserPosition = "0x0";
+    }
+
+    @Test
+        // Откройте страницу Selenide в Github
+        // Перейдите в раздел Wiki проекта
+        // Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+        // Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+
+    void gitHubTest() {
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $(".markdown-body").$(byText("Soft assertions")).click();
+        $(".markdown-body").shouldHave(Condition.text("Using JUnit5"));
+    }
 }
